4fad3df8693ed1335b7fee7fb4b0cf361d9b3d55
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-01-13 19:48:40 +0000

    Add logic for score remembering

--- Rugby Scorecard/index.js
+++ Rugby Scorecard/index.js
@@ -1,10 +1,13 @@
 let awayPoint = document.getElementById("away-score");
 let homePoint = document.getElementById("home-score");
 let homeTeam = document.getElementById("home-team");
-let awayTeam = document.getElementById("away-team")
+let awayTeam = document.getElementById("away-team");
+let homeData = document.getElementById("home-data");
+let awayData = document.getElementById("away-data");
 
 let homeScore = 0;
 let awayScore = 0;
+let point = "";
 
 function updateLeader() {
     if (homeScore > awayScore) {
@@ -20,55 +23,71 @@ function updateLeader() {
 }
 
 function homePlusTwo() {
+    point = "Con, "
     homeScore += 2;
     homePoint.textContent = homeScore;
     updateLeader();
+    homeData.textContent += point;
 }
 
 function homePlusThreePen() {
+    point = "Pen, "
     homeScore += 3;
     homePoint.textContent = homeScore;
     updateLeader();
+    homeData.textContent += point;
 }
 
 function homePlusThreeDrop() {
+    point = "Drop, "
     homeScore += 3;
     homePoint.textContent = homeScore;
     updateLeader();
+    homeData.textContent += point;
 }
 
 function homePlusFive() {
+    point = "Try, "
     homeScore += 5;
     homePoint.textContent = homeScore;
     updateLeader();
+    homeData.textContent += point;
     
 }
 
 function awayPlusTwo() {
+    point = "Con, "
     awayScore += 2;
     awayPoint.textContent = awayScore;
     updateLeader();
+    awayData.textContent += point;
     
 }
 
 function awayPlusThreePen() {
+    point = "Pen, "
     awayScore += 3;
     awayPoint.textContent = awayScore;
     updateLeader();
+    awayData.textContent += point;
     
 }
 
 function awayPlusThreeDrop() {
+    point = "Drop, "
     awayScore += 3;
     awayPoint.textContent = awayScore;
     updateLeader();
+    awayData.textContent += point;
     
 }
 
 function awayPlusFive() {
+    point = "Try, "
     awayScore += 5;
     awayPoint.textContent = awayScore;
     updateLeader();
+    awayData.textContent += point;
     
 }
 
@@ -78,5 +97,7 @@ function newGame() {
     homePoint.textContent = homeScore;
     awayPoint.textContent = awayScore;
     updateLeader();
+    homeData.textContent = "";
+    awayData.textContent = "";
 }
 
